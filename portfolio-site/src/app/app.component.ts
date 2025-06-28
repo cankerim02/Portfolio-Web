b5c841b65b802350f8ba72813340a40e5b52858b
cankerim02 <cankerim02@gmail.com>
2025-06-28 15:51:46 +0300

    tek sayfa içinde home about, projects ve contact bölümleri eklendi ve dinamik proje örnek listesi oluşturuldu

--- portfolio-site/src/app/app.component.ts
+++ portfolio-site/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../pages/footer/footer.component';
+import { HeaderComponent } from '../pages/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio-site';
+  projects = [
+    { title: 'Proje 1', description: 'Açıklama 1' },
+    { title: 'Proje 2', description: 'Açıklama 2' },
+    { title: 'Proje 3', description: 'Açıklama 3' },
+    { title: 'Proje 4', description: 'Açıklama 4' }
+  ];
 }
