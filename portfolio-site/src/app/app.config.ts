c572e9975c68ca774c5c61ea033876831f719d9d
cankerim02 <cankerim02@gmail.com>
2025-06-28 15:50:41 +0300

    config ayarları yapıldı

--- portfolio-site/src/app/app.config.ts
+++ portfolio-site/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    // importProvidersFrom(HttpClientModule) //eski HttpClientModule'u kullanmak yerine, SSR uyumlu hale getirmek için aşağıdaki satırı kullanıyoruz
+    provideHttpClient(withFetch()), //SSR uyumlu hale getirir
+    provideClientHydration()
+  ]
 };
