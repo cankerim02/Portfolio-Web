02376b259746c3e6a14acee8ba8aa4e9f50a0425
cankerim02 <cankerim02@gmail.com>
2025-07-19 15:42:58 +0300

    feat: hero component eklendi, dark mode ve görsel efektler tamamlandı

---
+++ portfolio-site/src/pages/hero/hero.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  name = 'Kerim Can';
+  job = 'Software Developer';
+}
