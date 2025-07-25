de8ecc939f387dcb279e6fe743e3b57d6f85953c
cankerim02 <cankerim02@gmail.com>
2025-07-25 22:11:15 +0300

    routing yapısı AppComponent üzerinden tek route olacak şekilde sadeleştirildi

--- portfolio-site/src/app/app.routes.ts
+++ portfolio-site/src/app/app.routes.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../pages/home/home.component';
-import { AboutComponent } from '../pages/about/about.component';
-import { ContactComponent } from '../pages/contact/contact.component';
-import { ProjectsComponent } from '../pages/projects/projects.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  {path: 'contact', component: ContactComponent},
-  {path: 'projects', component: ProjectsComponent},
-
+  { path: '', component: AppComponent },
 ];
