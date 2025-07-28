486749b554b232f30352f5fef829d3ceea2e9b0f
cankerim02 <cankerim02@gmail.com>
2025-07-28 21:13:26 +0300

    Revert "Api servisten dinamik olarak proje verilerinin çekilmesi için gerekli iş kuralları yazıldı."
    This reverts commit c33a66694458d9572df70147d6246186087d7075.

--- portfolio-site/src/pages/projects/projects.component.ts
+++ portfolio-site/src/pages/projects/projects.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import {ProjectService} from '../../services/project.service';
 import { Project } from '../../app/models/project.model';
-import { ProjectService } from '../../services/project.service';
+import { ProjectHttpService } from '../../services/project-http.service';
 
 
 
@@ -15,16 +15,45 @@ import { ProjectService } from '../../services/project.service';
 })
 export class ProjectsComponent  {
  showAll = false;
- projects: Project[] = [];
+ projects: Project[] = [
+    {
+      title: 'Portfolio Website',
+      description: 'My personal portfolio built with Angular and Tailwind CSS.',
+      image: '',
+      link: '#',
 
- constructor(private projectService: ProjectService) { }
-
- ngOnInit(): void {
-   this.projectService.getAllProject().subscribe({
-    next : (data) => this.projects = data,
-    error: (err) => console.error('Projeler yüklenirken hata:', err)
- })
-}
+    },
+    {
+      title: 'E-Commerce App',
+      description: 'A modern e-commerce platform with Angular frontend.',
+      image: '',
+      link: '#'
+    },
+    {
+      title: 'Blog Platform',
+      description: 'A blog site built using Angular and Node.js.',
+      image: '',
+      link: '#'
+    },
+     {
+      title: 'Blog Platform',
+      description: 'A blog site built using Angular and Node.js.',
+      image: '',
+      link: '#'
+    },
+     {
+      title: 'Blog Platform',
+      description: 'A blog site built using Angular and Node.js.',
+      image: '',
+      link: '#'
+    },
+     {
+      title: 'Blog Platform',
+      description: 'A blog site built using Angular and Node.js.',
+      image: '',
+      link: '#'
+    }
+  ];
    get displayedProjects(): Project[] {
     return this.showAll ? this.projects : this.projects.slice(0, 3);
   }
