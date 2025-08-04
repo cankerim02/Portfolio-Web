b6ef552849367da3c747338e0925647afae333e3
cankerim02 <cankerim02@gmail.com>
2025-08-04 20:16:33 +0300

    projeler apiden servisten dinamik çekilecek şekilde ayarlandı.

--- portfolio-site/src/pages/projects/projects.component.ts
+++ portfolio-site/src/pages/projects/projects.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import {ProjectService} from '../../services/project.service';
+import {ProjectService} from '../../services/project.service';
 import { Project } from '../../app/models/project.model';
-import { ProjectHttpService } from '../../services/project-http.service';
-
-
 
 @Component({
   selector: 'app-projects',
@@ -15,45 +12,16 @@ import { ProjectHttpService } from '../../services/project-http.service';
 })
 export class ProjectsComponent  {
  showAll = false;
- projects: Project[] = [
-    {
-      title: 'Portfolio Website',
-      description: 'My personal portfolio built with Angular and Tailwind CSS.',
-      image: '',
-      link: '#',
+ projects: Project[] = [];
 
-    },
-    {
-      title: 'E-Commerce App',
-      description: 'A modern e-commerce platform with Angular frontend.',
-      image: '',
-      link: '#'
-    },
-    {
-      title: 'Blog Platform',
-      description: 'A blog site built using Angular and Node.js.',
-      image: '',
-      link: '#'
-    },
-     {
-      title: 'Blog Platform',
-      description: 'A blog site built using Angular and Node.js.',
-      image: '',
-      link: '#'
-    },
-     {
-      title: 'Blog Platform',
-      description: 'A blog site built using Angular and Node.js.',
-      image: '',
-      link: '#'
-    },
-     {
-      title: 'Blog Platform',
-      description: 'A blog site built using Angular and Node.js.',
-      image: '',
-      link: '#'
-    }
-  ];
+ constructor(private projectService: ProjectService) { }
+
+ ngOnInit(): void {
+   this.projectService.getAllProject().subscribe({
+    next : (data) => this.projects = data,
+    error: (err) => console.error('Projeler yüklenirken hata:', err)
+ })
+}
    get displayedProjects(): Project[] {
     return this.showAll ? this.projects : this.projects.slice(0, 3);
   }
