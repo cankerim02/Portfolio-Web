71c46d46df7ce97cc00cc6af59ba0e889f2c7d38
cankerim02 <cankerim02@gmail.com>
2025-08-05 20:12:23 +0300

    app component  kodlaarı taşındı.

--- portfolio-site/src/app/app.component.ts
+++ portfolio-site/src/app/app.component.ts
@@ -1,38 +1,12 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../pages/footer/footer.component';
-import { HeaderComponent } from '../pages/header/header.component';
-import { CommonModule } from '@angular/common';
-import { HeroComponent } from '../pages/hero/hero.component';
-import { AboutComponent } from "../pages/about/about.component";
-import { ProjectsComponent } from "../pages/projects/projects.component";
-import { ContactComponent } from "../pages/contact/contact.component";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    FooterComponent,
-    HeroComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent
-    ],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  isDarkMode= false;
-
-toggleDarkMode() {
-  this.isDarkMode = !this.isDarkMode;
-
-  if (this.isDarkMode) {
-    document.documentElement.classList.add('dark');  // <html> elementine ekle
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
-}
-
-}
+export class AppComponent {}
