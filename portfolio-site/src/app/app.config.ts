0d4ef2e20edfa332a8aa33dd3ab7b106f408550a
cankerim02 <cankerim02@gmail.com>
2025-08-24 14:09:44 +0300

    Template-driven form desteğini açmak için forms modül eklendi.

--- portfolio-site/src/app/app.config.ts
+++ portfolio-site/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    // importProvidersFrom(HttpClientModule) //eski HttpClientModule'u kullanmak yerine, SSR uyumlu hale getirmek için aşağıdaki satırı kullanıyoruz
+    //  importProvidersFrom(HttpClientModule), //eski HttpClientModule'u kullanmak yerine, SSR uyumlu hale getirmek için aşağıdaki satırı kullanıyoruz
     provideHttpClient(withFetch()), //SSR uyumlu hale getirir
-    provideClientHydration()
+    provideClientHydration(),
+    importProvidersFrom(FormsModule)
   ]
 };
