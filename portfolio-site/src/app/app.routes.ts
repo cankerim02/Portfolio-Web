883ecf4c9791c7216db011b3ff2fd055aa027ad6
cankerim02 <cankerim02@gmail.com>
2025-09-19 23:30:01 +0300

    sayfa yönlendirmeleri yapıldı.

--- portfolio-site/src/app/app.routes.ts
+++ portfolio-site/src/app/app.routes.ts
@@ -1,6 +1,36 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+
+// Admin components
+import { AdminLayoutComponent } from '../pages/admin/admin-layout/admin-layout.component';
+import { DashboardComponent } from '../pages/admin/dashboard/dashboard.component';
+
+import { ContactsAdminComponent } from '../pages/admin/contacts-admin/contacts-admin.component';
+import { ProjectsAdminComponent } from '../pages/admin/project-admin/project-admin.component';
+import { LoginAdminComponent } from '../pages/admin/login-admin/login-admin.component';
+import { HomeLayoutComponent } from '../pages/home-layout/home-layout.component';
+import { AuthGuard } from './guards/authguard';
+
+
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+
+  // Login sayfası
+  {path: 'login', component: LoginAdminComponent},
+
+// PUBLIC
+
+    { path: '', component: HomeLayoutComponent },
+
+  // Admin routes
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    canActivate: [AuthGuard], // burada guard ekledik
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'projects', component: ProjectsAdminComponent },
+      { path: 'contacts', component: ContactsAdminComponent }
+    ]
+  },
+
 ];
