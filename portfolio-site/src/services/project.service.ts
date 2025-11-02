37ad8274a344d5fe983bd116e8e86672a077bc34
cankerim02 <cankerim02@gmail.com>
2025-11-02 12:33:02 +0300

    apiUrl alanı baseUrl sabit olacak şekilde ayarlandı.

--- portfolio-site/src/services/project.service.ts
+++ portfolio-site/src/services/project.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { Project } from '../app/models/project.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
- private apiUrl = 'https://localhost:44321/api/Project';
+ private apiUrl = `${environment.apiUrl}/Project`;
 
 
  constructor(private http:HttpClient) { }
 
  getAllProject():Observable<Project[]> {
-   return this.http.get<Project[]>(this.apiUrl);
+   return this.http.get<Project[]>(`${this.apiUrl}`);
  }
-
 }
