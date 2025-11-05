c859affc30d200f324edae62e5869ce7690eb198
cankerim02 <cankerim02@gmail.com>
2025-11-05 23:56:25 +0300

    browser animatons module ve  toastr import edildi

--- portfolio-site/src/app/app.config.ts
+++ portfolio-site/src/app/app.config.ts
@@ -6,11 +6,25 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+//  Animasyon ve Toastr için importlar
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
+
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     //  importProvidersFrom(HttpClientModule), //eski HttpClientModule'u kullanmak yerine, SSR uyumlu hale getirmek için aşağıdaki satırı kullanıyoruz
     provideHttpClient(withFetch()), //SSR uyumlu hale getirir
     provideClientHydration(),
-    importProvidersFrom(FormsModule)
+    provideToastr(
+      {
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+        timeOut: 3000,
+      }),
+    importProvidersFrom(FormsModule,
+      BrowserAnimationsModule,
+    )
   ]
 };
